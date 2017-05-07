155590ff0b5e346ddb6bbe2aba2e59d07a4c32c7
Aatif Shah <aatif.de.shah@gmail.com>
2017-05-07 04:25:05 -0700

    login works with db

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,8 +1,10 @@
 'use strict';
 // load the Cloudant library
-
+var cloudantURL = "https://azincencorrioduchignoody:7d392fbae6755d3aab27bdb441c2b6a1ae879194@38255844-f351-4ae0-9d53-5774023e3cf4-bluemix.cloudant.com"
 var Cloudant = require('cloudant'),
-  cloudant = Cloudant({url: "https://azincencorrioduchignoody:7d392fbae6755d3aab27bdb441c2b6a1ae879194@mydomain.cloudant.com"}),
+  cloudant = Cloudant({
+    url: cloudantURL
+  }),
   db = cloudant.db.use("users");
 
 // create a document
@@ -16,35 +18,30 @@ var createDocument = function(newUser, callback) {
   });
 };
 
-var HttpClient = function() {
-  this.get = function(aUrl, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", aUrl, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status == 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status);
-      }
-    };
-    xhr.send();
-  }
-}
-
 //login
-exports.get_user = function(req, res){
+exports.get_user = function(req, res) {
   var username = req.params.username;
   var password = req.params.password;
 
-  var client = new HttpClient();
   //if db contains username, check password
-
-  if()
+  db.find({
+    "selector": {
+      "$and": [{
+          "username": username
+        },
+        {
+          "password": password
+        }
+      ]
+    }
+  }, function(er, result){
+      console.log(er);
+      console.log(result);
+      res.json(result);
+  })
 }
 
 //
-exports.get_match = function(req, res){
+exports.get_match = function(req, res) {
   console.log("WOOTd!")
 }
