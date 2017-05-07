def762252da40c543ea3b1c2e7582ce937b52337
clarencechen <clarencechenct@gmail.com>
2017-05-07 00:12:44 -0700

    video call

--- a.js
+++ a.js
@@ -1,26 +1,66 @@
 var peer = new Peer({key: 'lwjd5qra8257b9'});
-var conn = peer.connect($('#peerid').val());
-
+var mediapromise = null;
 peer.on('open', function(id) {
   $('#myid').text('My peer ID is: ' + id);
-
-});
+  $('body').on('click', '#call', function(e) {
+    var peerid = $('#peerid').val()
+    call(peerid)
+  })
+  $('body').on('click', '#send', function(e) {
+    $('#send').attr("disabled", "disabled")
+    $('#send').text('connecting...')
+    var peerid = $('#peerid').val()
+    peer.connect(peerid, {metadata : 0})
+  })
+})
 
 peer.on('connection', function(conn) {
-  console.log('I got connected');
+  //receiver
+  if(conn.options.metadata === 0)
+  {
+    $('#send').attr("disabled", "disabled")
+    $('#send').text('connecting back...')
+    var peerid = conn.peer;
+    peer.connect(peerid, {metadata : 1})
+  }
+  //initiator
+  else if(conn.options.metadata === 1)
+  {
+    conn.send({twoway : true})
+    $('#send').text('send message')
+    $('#send').removeAttr("disabled")
+  }
   conn.on('open', function() {
     // Receive messages
     conn.on('data', function(data) {
-      console.log('Received', data);
-    });
-
+      if(data.twoway)
+      {
+        $('#send').text('send message')
+        $('#send').removeAttr("disabled")
+      }
+      else if(data.message)
+      {
+        $('#responses').append('<p>' + data + '</p>')
+        conn.send({success : true})
+      }
+      else if(data.success)
+      {
+        $('#send').text('send message')
+        $('#send').removeAttr("disabled")
+      }
+    })
     // Send messages
-
-    //Start Call
-    call()
-  });
-});
-
+  })
+  $('body').on('click', '#send', function(e) {
+    if($('#message').val() !== '')
+    {
+      $('#send').text('on its way...')
+      $('#send').attr("disabled", "disabled")        
+      conn.send({message : $('#message').val()})
+    }
+    $('#message').val('')
+  })
+})
 
 
 
@@ -29,11 +69,15 @@ function display(remote) {
   video.srcObject = remote;
   video.onloadedmetadata = function(e) {video.play();}
 }
-function call() {
+function call(peerid) {
+  if(mediapromise !== null)
+  {
+    console.log("already in call")
+    return;
+  }  
   console.log("prepare for call")
-  var mediapromise = navigator.mediaDevices.getUserMedia({audio : true, video :true});
+  mediapromise = navigator.mediaDevices.getUserMedia({audio : true, video : true});
   mediapromise.then(function(stream) {
-    var peerid = $('#peerid').val()
     var call = peer.call(peerid, stream);
     $('#call').text('end call')
     call.on('stream', function(remote) {
@@ -49,7 +93,8 @@ peer.on('error', function(e) {
   console.log(e.type);
 })
 peer.on('call', function(call) {
-  var mediapromise = navigator.mediaDevices.getUserMedia({audio : true, video : true});
+  console.log('receiving call')
+  mediapromise = navigator.mediaDevices.getUserMedia({audio : true, video : true});
   mediapromise.then(function(stream) {
     // Answer the call, providing our MediaStream
     call.answer(stream);
