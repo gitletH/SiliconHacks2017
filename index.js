2c15023692c2f4d02ff9ee2d2c48f479f2ed51b8
Silicon Hacks <SiliconHacks@e1z1r4p1.42.us.org>
2017-05-07 06:26:53 -0700

    it serves static content too

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    path = require('path');
     app = express(),
     port = 3000,
     bodyParser = require('body-parser');
@@ -6,6 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var routes = require('./api/routes/routes.js');
 routes(app);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port);
 
