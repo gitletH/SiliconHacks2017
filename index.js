b0718552525b6a3f0a06a89df3a128b5afdcddd0
clarencechen <clarencechenct@gmail.com>
2017-05-07 06:18:47 -0700

    fuck cookies don't eat them

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = 3000,
     bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
