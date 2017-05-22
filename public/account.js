2b5f4fa3aaf72fc81d429b5ed420103bf9ca776a
clarencechen <clarencechenct@gmail.com>
2017-05-21 22:25:07 -0700

    fix up html

---
+++ public/account.js
@@ -0,0 +1,18 @@
+var isToggled = false;
+function toggle() {
+	console.log(isToggled)
+	if (isToggled)
+	{
+		isToggled = false;
+		document.getElementById("sidebar").style.right = "-100%";
+	}
+	else
+	{
+		isToggled = true;
+		document.getElementById("sidebar").style.right = "0";
+	}
+}
+function advance() {
+	$('#account').toggle(600);
+	$('#background').toggle(600);
+}
